b100478ac599307b4275cd250fdf9daf7f49cfd6
Hanaa-Kamel <hanaa.kamel.a@gmail.com>
2021-08-28 09:15:01 +0200

    updateC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvComponent } from './tv/tv.component';
+import { NetworkComponent } from './network/network.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MoviesComponent,
+    TvComponent,
+    NetworkComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
