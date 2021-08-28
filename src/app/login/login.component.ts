a8eae444fca69179e53a47088928c4ce569f014e
Hanaa-Kamel <hanaa.kamel.a@gmail.com>
2021-08-28 15:38:41 +0200

    authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,7 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  error: string = '';
+
+  loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('^[A-Z][a-z0-9]{3,8}$'),
+    ])
+  })
+
+  constructor(private _AuthService:AuthService , private _Router:Router) {}
+
+  submitLoginForm(loginForm: FormGroup) {
+    this._AuthService.login(loginForm.value).subscribe((response)=>{
+
+      if(response.message == 'success')
+      {
+        localStorage.setItem('userToken' , response.token);
+        this._AuthService.saveCurrentUser()
+        this._Router.navigate(['/home'])
+      }
+      else
+      {
+        this.error = response.message
+      }
+
+    })
+  }
+
 
   ngOnInit(): void {
   }
