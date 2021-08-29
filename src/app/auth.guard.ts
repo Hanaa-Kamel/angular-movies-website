ce399c79b405168d28214e0b1b981174867bd7c4
Hanaa-Kamel <hanaa.kamel.a@gmail.com>
2021-08-29 08:15:46 +0200

    update

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if(this._AuthService.currenUser.getValue() != null )
+    if(this._AuthService.currenUser.getValue() != null && localStorage.getItem('userToken') != null )
     {
       return true;
     }
