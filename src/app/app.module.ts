5e492abfe803af3dd46f5625b1700eb026671357
Hanaa-Kamel <hanaa.kamel.a@gmail.com>
2021-08-29 08:13:08 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { MoviesComponent } from './movies/movies.component';
@@ -16,6 +17,8 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { PeopleComponent } from './people/people.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
@@ -29,13 +32,18 @@ import { ReactiveFormsModule } from '@angular/forms';
     RegisterComponent,
     NavbarComponent,
     NotfoundComponent,
-    PeopleComponent
+    PeopleComponent,
+    HeaderComponent,
+
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule ,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
